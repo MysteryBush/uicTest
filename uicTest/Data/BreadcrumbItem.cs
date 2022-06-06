a5d1c029d9fbae84f2ed11ae101555fcf600a55f
Tonnam Somchanakij <tonnam.dev@gmail.com>
2022-06-06 12:48:47 +0700

    feat <Breadcrumb /> with .split() and create items with for loop - feat public string pageAddress are used on each page for Breadcrumb - removed BreadcrumbItem.cs

--- uicTest/Data/BreadcrumbItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace uicTest.Data
-{
-    public class BreadcrumbItem
-    {
-        public string? Title { get; set; }
-        public string? Address { get; set; }
-        public bool Activated { get; set; }
-    }
-}
