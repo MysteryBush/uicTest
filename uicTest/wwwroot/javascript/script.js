07df27627b55d949d9ba86eef65f50a21b1f156c
ScorchedTrace <Songkhla0@gmail.com>
2022-06-14 09:25:14 +0700

    feat hide/show navbar on scroll - fix remove span class that shows a plus sign on one of the navbar item - fix make navbar have z-index of 8

--- uicTest/wwwroot/javascript/script.js
+++ uicTest/wwwroot/javascript/script.js
@@ -28,6 +28,17 @@ function swipeUp(div) {
     var element = div;
 }
 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
 /* On page loaded, scroll to top */
 // https://stackoverflow.com/a/26837814
